73f5aa6a89027be5c402ed15f4ade4a0862f3f3c
Abdullah Al Mamun <abdullahrens@gmail.com>
2025-03-23 23:59:03 +0100

    works till the end with some name problems

--- ctrlFind.py
+++ ctrlFind.py
@@ -6,7 +6,7 @@ import re
 
 pp = pprint.PrettyPrinter(indent=4)
 
-source = requests.get("https://docs.fcdo.gov.uk/docs/UK-Sanctions-List.html").text[:26000]
+source = requests.get("https://docs.fcdo.gov.uk/docs/UK-Sanctions-List.html").text[:]
 
 soup = BS(source, "lxml")
 
@@ -59,7 +59,7 @@ def dictify(arr):
 	i = 0
 	individualName = ""
 	while i < len(arr):
-		# print ('elemtn: ' + arr[i])
+		print ('elemtn: ' + arr[i])
 		
 		uniqueIdMatch = re.fullmatch("Unique ID:", arr[i])
 		entityTypeMatch = re.fullmatch("Entity", arr[i])
@@ -67,44 +67,35 @@ def dictify(arr):
 		
 		if (uniqueIdMatch):
 			tempD["id"] = arr[i + 1]
+			tempD["type"] = arr[i + 2]
 			i += 2
 
-		elif (entityTypeMatch or individualTypeMatch):
-			print('got into entity')
-			print (arr[i])
-			tempD["type"] = arr[i]
-			i += 1
-
-		#Since everything hereon has a lookahead
+		#Since everything here on has a lookahead
 		elif ((i + 1) < len(arr)):
+			print ('tried in lookahead')
 			a = arr[i].split('Name:')
 			pattern = "Name:"
 			t = re.fullmatch(pattern, arr[i])
 			if (t):
-				# print ('Got into name')
-				if tempD["type"] == "Entity":
-					if "name" not in tempD:
-						tempD["name"] = [arr[i + 1]]
-					else:
-						tempD["name"].append(arr[i + 1])
-						i += 1
-				
+				print ('Got into name')
+				print (tempD)
+
 				if tempD["type"] == "Individual":
 					individualName = ""
 					j = i + 1
-					# print ('in individual name check')
+					print ('in individual name check')
 					if "name" not in tempD:
 						while arr[j] != "Name Type:":
 
-							print (j)
+							# print (j)
 							print (arr[j])
 							individualName += arr[j] + " "
 
 							j += 1
-						# print ('done with frst ever name: ' + individualName)
+						print ('done with frst ever name: ' + individualName)
 						print (tempD)
 						tempD["name"] = [individualName.strip()]
-						print (tempD)
+						# print (tempD)
 					else:
 						# print ('while name exists in dict')
 						print (arr[j])
@@ -114,6 +105,16 @@ def dictify(arr):
 						# print ('done with additonal names: ' + individualName)
 						tempD["name"].append(individualName.strip())
 					i = j
+					print (i)
+
+				if tempD["type"]:
+					if "name" not in tempD:
+						tempD["name"] = [arr[i + 1]]
+					else:
+						tempD["name"].append(arr[i + 1])
+						i += 1
+				
+				
 
 
 			if ("Address Country:" in arr[i]):
@@ -124,34 +125,27 @@ def dictify(arr):
 						tempD["country"].append(arr[i + 1])
 				i += 1
 
-		
-			if ("Unique ID:" in arr[i+1]):
-				print (arr[i+1])
-				print ('found unique id: ' + arr[i+2])
-				print (tempD)
+			
+			#Addtional check before doing id lookahead
+			if (i != (len(arr) - 1) and "Unique ID:" in arr[i+1]):
+
 				globalDB.append(tempD)
 				tempD = {}
 				i += 1
 
+			#For the very last element	
+			elif ((i + 1) >= len(arr)):
+
+				globalDB.append(tempD)
+				tempD = {}
+				i += 1
+
+			#normal increment
 			else:
 				i += 1
 
 			continue
 
-		#For the very last element	
-		elif ((i + 1) >= len(arr)):
-			print ('came in final')
-			print (tempD)
-			globalDB.append(tempD)
-			tempD = {}
-			i += 1
-			continue
-			break
-
-		else:
-			print ('normal increment')
-			i += 1
-			continue
 
 print(processedElements[-1])
 dictify(processedElements)
